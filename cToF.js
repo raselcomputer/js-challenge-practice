4e371a0d1c0ba23e53cae61c25381b2d8c76e601
rasel computer <raselcomputer75@gmail.com>
2021-08-05 18:20:05 +0600

    practice added

---
+++ cToF.js
@@ -0,0 +1,25 @@
+// function cenciusToFarenheit(celcius){
+//     let farenheit = cenciusToFarenheit(celcius) * 9 /5 + 32
+//     return farenheit; 
+// }
+// const myFarenheit = cenciusToFarenheit(2);
+// console.log(myFarenheit);
+
+
+// function cToF(celcius){
+//     var cTemp = celcius;
+//     var cToF = cTemp * 9 / 5 + 32;
+//     return cToF;
+// }
+// var myCelcius = cToF(0);
+// console.log(myCelcius);   
+
+
+
+function cToF(celsius){
+    var cTemp = celsius;
+    var cToFaren = cTemp * 9 / 5 + 32;
+    return cToFaren;
+}
+var myCelsius = cToF(5);
+console.log(myCelsius);
---
+++ result.js
@@ -0,0 +1,24 @@
+let marks = 70;
+
+if(marks >= 80 && marks <= 100){
+    console.log('A+');
+}
+    else if(marks >= 70 && marks < 80){
+        console.log('A');
+    }
+    else if(marks >= 60 && marks < 70){
+        console.log('A-');
+    }
+    else if(marks >= 50 && marks < 60){
+        console.log('B');
+    }
+    else if(marks >= 40 && marks < 50){
+        console.log('C');
+    }
+    else if(marks >= 33 && marks < 40){
+        console.log('D');
+    }
+
+else{
+    console.log('Fail');
+}
